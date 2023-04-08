67d7c8c407a3b608a6464db5bb940c0be4d9007b
927TriponBriana <91745099+917TriponBriana@users.noreply.github.com>
2023-04-08 17:08:24 +0300

    change

--- dental_clinic_react_frontend/src/components/AppHome.tsx
+++ dental_clinic_react_frontend/src/components/AppHome.tsx
@@ -8,7 +8,7 @@ export const AppHome = () => {
 
             <Container maxWidth="xl">
                 <Typography variant="h1" component="h1" gutterBottom>
-                    Welcome!
+                    Welcome! BlaBla...
                 </Typography>
             </Container>
         </React.Fragment>
--- dental_clinic_react_frontend/src/constants.ts
+++ dental_clinic_react_frontend/src/constants.ts
@@ -1,4 +1,5 @@
 const PROD_BACKEND_API_URL = "ec2-16-170-108-25.eu-north-1.compute.amazonaws.com/app1/clin";
+// const PROD_BACKEND_API_URL = "ec2-16-170-133-56.eu-north-1.compute.amazonaws.com/app1/clin";
 const DEV_BACKEND_API_URL = "http://127.0.0.1:8000/app1/clin";
 
 export const BACKEND_API_URL = 
