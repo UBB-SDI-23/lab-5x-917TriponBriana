65433783a5a3b025b83c02aaa483c96b440e1a03
TriponBriana <91745099+927TriponBriana@users.noreply.github.com>
2023-04-08 20:08:15 +0300

    Add files via upload

---
+++ dental_clinic_backend/app1/Views/MedicationViews.py
@@ -0,0 +1,64 @@
+from django.db.models import Avg, Count, OuterRef
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from ..models import Patient, Dentist, Consultation, Medication, MedicationDentist
+from ..serializer import PatientSerializer, DentistSerializer, ConsultationSerializer, MedicationSerializer, \
+    MedicationDentistSerializer, PatientIdSerializer
+from rest_framework import generics
+
+class MedicationDetail(APIView):
+    def get(self, request):
+        obj = Medication.objects.all()
+        serializer = MedicationSerializer(obj, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = MedicationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+
+class MedicationInfo(APIView):
+    def get(self, request, id):
+        try:
+            obj = Medication.objects.get(id=id)
+        except Medication.DoesNotExist:
+            msg = {"msg": "Not found"}
+            return Response(msg, status=status.HTTP_404_NOT_FOUND)
+        serializer = MedicationSerializer(obj)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, id):
+        try:
+            obj = Medication.objects.get(id=id)
+        except Medication.DoesNotExist:
+            msg = {"msg": "Not found"}
+            return Response(msg, status=status.HTTP_404_NOT_FOUND)
+        serializer = MedicationSerializer(obj, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_205_RESET_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def patch(self, request, id):
+        try:
+            obj = Medication.objects.get(id=id)
+        except Medication.DoesNotExist:
+            msg = {"msg": "Not found"}
+            return Response(msg, status=status.HTTP_404_NOT_FOUND)
+        serializer = MedicationSerializer(obj, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_205_RESET_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        try:
+            obj = Medication.objects.get(id=id)
+        except Medication.DoesNotExist:
+            msg = {"msg": "Not found"}
+            return Response(msg, status=status.HTTP_404_NOT_FOUND)
+        obj.delete()
+        return Response({"msg" : "Deleted"}, status=status.HTTP_204_NO_CONTENT)
