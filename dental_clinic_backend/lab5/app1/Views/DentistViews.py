54a7f0dfb57a6d017f9e9f7506d867ee23180a4f
927TriponBriana <91745099+917TriponBriana@users.noreply.github.com>
2023-04-08 20:32:38 +0300

    .

---
+++ dental_clinic_backend/lab5/app1/Views/DentistViews.py
@@ -0,0 +1,64 @@
+from django.db.models import Avg, Count, OuterRef
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from ..models import Patient, Dentist, Consultation, Medication, MedicationDentist
+from ..serializer import PatientSerializer, DentistSerializer, ConsultationSerializer, MedicationSerializer, \
+    MedicationDentistSerializer, PatientIdSerializer
+from rest_framework import generics
+class DentistDetail(APIView):
+    def get(self, request):
+        obj = Dentist.objects.all()
+        serializer = DentistSerializer(obj, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = DentistSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+
+class DentistInfo(APIView):
+    def get(self, request, id):
+        try:
+            obj = Dentist.objects.get(id=id)
+        except Dentist.DoesNotExist:
+            msg = {"msg" : "Not found"}
+            return Response(msg, status=status.HTTP_404_NOT_FOUND)
+        serializer = DentistSerializer(obj)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, id):
+        try:
+            obj = Dentist.objects.get(id=id)
+        except Dentist.DoesNotExist:
+            msg = {"msg" : "Not found"}
+            return Response(msg, status=status.HTTP_404_NOT_FOUND)
+        serializer = DentistSerializer(obj, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_205_RESET_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, id):
+        try:
+            obj = Dentist.objects.get(id=id)
+        except Dentist.DoesNotExist:
+            msg = {"msg": "Not found"}
+            return Response(msg, status=status.HTTP_404_NOT_FOUND)
+        serializer = DentistSerializer(obj, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_205_RESET_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        try:
+            obj = Dentist.objects.get(id=id)
+        except Dentist.DoesNotExist:
+            msg = {"msg": "Not found"}
+            return Response(msg, status=status.HTTP_404_NOT_FOUND)
+        obj.delete()
+        return Response({"msg": "Deleted"}, status=status.HTTP_204_NO_CONTENT)
