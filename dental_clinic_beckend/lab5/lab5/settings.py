1c6d5cf1e4b69b03e5ea76e5c393c5154579a670
927TriponBriana <91745099+917TriponBriana@users.noreply.github.com>
2023-04-09 00:16:55 +0300

    .

--- dental_clinic_beckend/lab5/lab5/settings.py
+++ dental_clinic_beckend/lab5/lab5/settings.py
@@ -15,8 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-REAL_BASE_DIR = Path(__file__).resolve().parent.parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -74,7 +72,7 @@ ROOT_URLCONF = 'lab5.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(REAL_BASE_DIR, 'dental_clinic_react_frontend', 'build')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -136,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(REAL_BASE_DIR, 'dental_clinic_react_frontend', 'build', 'static')]
+#STATICFILES_DIRS = [os.path.join(REAL_BASE_DIR, 'dental_clinic_react_frontend', 'build', 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
