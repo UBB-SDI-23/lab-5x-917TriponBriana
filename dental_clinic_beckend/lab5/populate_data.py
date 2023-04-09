2707d1185b8eba72baab048160f5424667f817a8
927TriponBriana <91745099+917TriponBriana@users.noreply.github.com>
2023-04-09 16:36:51 +0300

    .

---
+++ dental_clinic_beckend/lab5/populate_data.py
@@ -0,0 +1,17 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lab5.settings')
+django.setup()
+
+from app1.models import Patient
+
+if __name__ == '__main__':
+    from faker import Faker
+
+    fake = Faker()
+    n = 10000
+    for _ in range(n):
+        Patient.objects.create(patient_first_name=fake.patient_first_name(), patient_last_name=fake.patient_last_name(), patient_age=fake.patient_age(), patient_country=fake.patient_country(), patient_consultation=fake.patient_consultation())
+
+
