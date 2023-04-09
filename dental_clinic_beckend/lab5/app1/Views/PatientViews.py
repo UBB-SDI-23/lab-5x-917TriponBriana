b49d70e62c41181fb03be1f1b043bfc311bc8a41
927TriponBriana <91745099+917TriponBriana@users.noreply.github.com>
2023-04-09 15:24:06 +0300

    .

--- dental_clinic_beckend/lab5/app1/Views/PatientViews.py
+++ dental_clinic_beckend/lab5/app1/Views/PatientViews.py
@@ -8,9 +8,9 @@ from ..serializer import PatientSerializer, DentistSerializer, ConsultationSeria
 from rest_framework import generics
 
 
-class PatientDetail(generics.ListAPIView):
-    queryset = Patient.objects.all()
-    serializer_class = PatientSerializer
+class PatientDetail(APIView):
+    # queryset = Patient.objects.all()
+    # serializer_class = PatientSerializer
 
     def get(self, request):
         obj = Patient.objects.all()
@@ -25,9 +25,9 @@ class PatientDetail(generics.ListAPIView):
         return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
 
 
-class PatientInfo(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Patient.objects.all()
-    serializer_class = PatientSerializer
+class PatientInfo(APIView):
+    # queryset = Patient.objects.all()
+    # serializer_class = PatientSerializer
 
     def get(self, request, id):
         try:
@@ -72,9 +72,9 @@ class PatientInfo(generics.RetrieveUpdateDestroyAPIView):
         return Response({"msg" : "Deleted"}, status=status.HTTP_204_NO_CONTENT)
 
 
-class PatientIds(generics.ListAPIView):
-    queryset = Patient.objects.all()
-    serializer_class = PatientIdSerializer
+class PatientIds(APIView):
+    # queryset = Patient.objects.all()
+    # serializer_class = PatientIdSerializer
 
     def get(self, request):
         obj = Patient.objects.all()
--- dental_clinic_beckend/lab5/lab5/settings.py
+++ dental_clinic_beckend/lab5/lab5/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -93,8 +96,12 @@ WSGI_APPLICATION = 'lab5.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get('DB_ENGINE'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
