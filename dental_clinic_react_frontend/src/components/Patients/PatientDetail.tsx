4f51506995a107210d32ebedf510e25e20d5b3ae
927TriponBriana <91745099+917TriponBriana@users.noreply.github.com>
2023-04-09 23:07:41 +0300

    .

--- dental_clinic_react_frontend/src/components/Patients/PatientDetail.tsx
+++ dental_clinic_react_frontend/src/components/Patients/PatientDetail.tsx
@@ -2,10 +2,11 @@ import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom"
 import { Patient } from "../../models/Patient";
 import { BACKEND_API_URL } from "../../constants";
-import { Card, CardActions, CardContent, Container, IconButton } from "@mui/material";
+import { Box, Card, CardActions, CardContent, Container, IconButton } from "@mui/material";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import { PatientFull } from "../../models/PatientFull";
 
 export const PatientDetail = () => {
     const {patientId} = useParams();
@@ -23,27 +24,28 @@ export const PatientDetail = () => {
 
     return (
         <Container>
-            <Card>
-                <CardContent>
-                    <IconButton component={Link} sx={{ mr: 3}} to={`/patient`}>
-                        <ArrowBackIcon />
-                    </IconButton>{" "}
-                    <h1 style={{textAlign:"center"}}>Patient Details</h1>
-                    <p style={{textAlign:"left"}}>First Name: {patient?.patient_first_name}</p>
-                    <p style={{textAlign:"left"}}>Last Name: {patient?.patient_last_name}</p>
-                    <p style={{textAlign:"left"}}>Age: {patient?.patient_age}</p>
-                    <p style={{textAlign:"left"}}>Country: {patient?.patient_country}</p>
-                    <p style={{textAlign:"left"}}>Consultation: {patient?.patient_consultation}</p>
-                </CardContent>
-                <CardActions>
-                    <IconButton component={Link} sx={{ mr: 3}} to={`/patient/${patientId}/edit`}>
-                        <EditIcon />
-                    </IconButton>
-                    <IconButton component={Link} sx={{ mr: 3 }} to={`/patient/${patientId}/delete`}>
-                        <DeleteForeverIcon sx={{ color: "red" }} />
-                    </IconButton>
-                </CardActions>
-            </Card>
-        </Container>
+        <Card>
+            <CardContent>
+                <IconButton component={Link} sx={{ mr: 3 }} to={`/patient`}>
+                    <ArrowBackIcon />
+                </IconButton>{" "}
+                <h1 style={{textAlign:"center"}}>Patient Details</h1>
+                <p style={{textAlign:"left"}}>First Name: {patient?.patient_first_name}</p>
+                <p style={{textAlign:"left"}}>Last Name: {patient?.patient_last_name}</p>
+                <p style={{textAlign:"left"}}>Age: {patient?.patient_age}</p>
+                <p style={{textAlign:"left"}}>Country: {patient?.patient_country}</p>
+                <p style={{textAlign:"left"}}>Consultation: {patient?.patient_consultation}</p>
+            </CardContent>
+            <CardActions>
+                <IconButton component={Link} sx={{ mr: 3 }} to={`/patient/${patientId}/edit`}>
+                    <EditIcon />
+                </IconButton>
+
+                <IconButton component={Link} sx={{ mr: 3 }} to={`/patient/${patientId}/delete`}>
+                    <DeleteForeverIcon sx={{ color: "red" }} />
+                </IconButton>
+            </CardActions>
+        </Card>
+    </Container>
     );
 };
---
+++ dental_clinic_react_frontend/src/models/PatientFull.ts
@@ -0,0 +1,8 @@
+export interface PatientFull {
+    id? : number;
+    patient_first_name : string;
+    patient_last_name : string;
+    patient_age : number;
+    patient_country : string;
+    patient_consultation : string;
+}
