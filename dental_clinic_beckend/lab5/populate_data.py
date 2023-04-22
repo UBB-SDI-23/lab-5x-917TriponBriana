4bca72b0b8c6ebd9cc862b40c41e58bc019339a6
927TriponBriana <91745099+917TriponBriana@users.noreply.github.com>
2023-04-22 17:49:10 +0300

    .

--- dental_clinic_beckend/lab5/populate_data.py
+++ dental_clinic_beckend/lab5/populate_data.py
@@ -1,4 +1,6 @@
 import os
+import random
+
 import django
 
 
@@ -8,10 +10,21 @@ django.setup()
 from app1.Models.PatientModels import Patient
 
 
+# if __name__ == '__main__':
+#     from faker import Faker
+#
+#     fake = Faker()
+#     n = 10000
+#     for _ in range(n):
+#         Patient.objects.create(patient_first_name=fake.patient_first_name(), patient_last_name=fake.patient_last_name(), patient_age=fake.patient_age(), patient_country=fake.patient_country(), patient_consultation=fake.patient_consultation())
+
 if __name__ == '__main__':
     from faker import Faker
 
     fake = Faker()
-    n = 10000
-    for _ in range(n):
-        Patient.objects.create(patient_first_name=fake.patient_first_name(), patient_last_name=fake.patient_last_name(), patient_age=fake.patient_age(), patient_country=fake.patient_country(), patient_consultation=fake.patient_consultation())
+    n = 1000000
+    with open('populate_patient.csv', 'w') as f:
+        # f.write('INSERT INTO patient(patient_first_name, patient_last_name, patient_age, patient_country, patient_consultation) VALUES \n')
+        for i in range(n):
+            f.write("'" + fake.name() + "'," + fake.name() + "'," + random.randint(2, 100).__str__() + ",'" + fake.name() + ",'" + fake.name() + '\n')
+            print(i, "/", n)
