d8482882d0ccdd4639f31f5d388768da59bc556b
927TriponBriana <91745099+917TriponBriana@users.noreply.github.com>
2023-04-23 13:47:40 +0300

    .

--- dental_clinic_beckend/lab5/populate_data.py
+++ dental_clinic_beckend/lab5/populate_data.py
@@ -1,30 +1,30 @@
-import os
-import random
-
-import django
-
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lab5.settings')
-django.setup()
-
-from app1.Models.PatientModels import Patient
-
-
+# import os
+# import random
+#
+# import django
+#
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lab5.settings')
+# django.setup()
+#
+# from app1.Models.PatientModels import Patient
+#
+#
+# # if __name__ == '__main__':
+# #     from faker import Faker
+# #
+# #     fake = Faker()
+# #     n = 10000
+# #     for _ in range(n):
+# #         Patient.objects.create(patient_first_name=fake.patient_first_name(), patient_last_name=fake.patient_last_name(), patient_age=fake.patient_age(), patient_country=fake.patient_country(), patient_consultation=fake.patient_consultation())
+#
 # if __name__ == '__main__':
 #     from faker import Faker
 #
 #     fake = Faker()
-#     n = 10000
-#     for _ in range(n):
-#         Patient.objects.create(patient_first_name=fake.patient_first_name(), patient_last_name=fake.patient_last_name(), patient_age=fake.patient_age(), patient_country=fake.patient_country(), patient_consultation=fake.patient_consultation())
-
-if __name__ == '__main__':
-    from faker import Faker
-
-    fake = Faker()
-    n = 1000000
-    with open('populate_patient.csv', 'w') as f:
-        # f.write('INSERT INTO patient(patient_first_name, patient_last_name, patient_age, patient_country, patient_consultation) VALUES \n')
-        for i in range(n):
-            f.write("'" + fake.name() + "'," + fake.name() + "'," + random.randint(2, 100).__str__() + ",'" + fake.name() + ",'" + fake.name() + '\n')
-            print(i, "/", n)
+#     n = 1000000
+#     with open('populate_patient.csv', 'w') as f:
+#         # f.write('INSERT INTO patient(patient_first_name, patient_last_name, patient_age, patient_country, patient_consultation) VALUES \n')
+#         for i in range(n):
+#             f.write("'" + fake.name() + "'," + fake.name() + "'," + random.randint(2, 100).__str__() + ",'" + fake.name() + ",'" + fake.name() + '\n')
+#             print(i, "/", n)
