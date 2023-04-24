1adf0f85b3f9482f4dc4f621eb782e77aa2472f5
927TriponBriana <91745099+917TriponBriana@users.noreply.github.com>
2023-04-24 12:45:42 +0300

    .

--- dental_clinic_beckend/lab5/populate_data.py
+++ dental_clinic_beckend/lab5/populate_data.py
@@ -1,22 +1,22 @@
-import os
-import random
-
-import django
-
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lab5.settings')
-django.setup()
-
-from app1.Models.PatientModels import Patient
-
-
-if __name__ == '__main__':
-    from faker import Faker
-
-    fake = Faker()
-    n = 10000
-    for _ in range(n):
-        Patient.objects.create(patient_first_name=fake.patient_first_name(), patient_last_name=fake.patient_last_name(), patient_age=fake.patient_age(), patient_country=fake.patient_country(), patient_consultation=fake.patient_consultation())
+# import os
+# import random
+#
+# import django
+#
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lab5.settings')
+# django.setup()
+#
+# from app1.Models.PatientModels import Patient
+#
+#
+# if __name__ == '__main__':
+#     from faker import Faker
+#
+#     fake = Faker()
+#     n = 10000
+#     for _ in range(n):
+#         Patient.objects.create(patient_first_name=fake.patient_first_name(), patient_last_name=fake.patient_last_name(), patient_age=fake.patient_age(), patient_country=fake.patient_country(), patient_consultation=fake.patient_consultation())
 
 # if __name__ == '__main__':
 #     from faker import Faker
