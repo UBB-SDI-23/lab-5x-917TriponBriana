636b8379340e84f297ce3d7dcb8804fd8c6388bd
927TriponBriana <91745099+917TriponBriana@users.noreply.github.com>
2023-05-06 15:10:16 +0300

    .

--- dental_clinic_react_frontend/src/constants.ts
+++ dental_clinic_react_frontend/src/constants.ts
@@ -1,9 +1,9 @@
 //const PROD_BACKEND_API_URL = "ec2-16-170-108-25.eu-north-1.compute.amazonaws.com/app1/clin"; 
-//https://dental-clinic-app.jumpingcrab.com/app1/clin
 //const PROD_BACKEND_API_URL = "https://dental-clinic-app.mooo.com/app1/clin";
 //http://ec2-16-170-164-154.eu-north-1.compute.amazonaws.com:8000/api/clin
-// const PROD_BACKEND_API_URL = "/clinic";
-const PROD_BACKEND_API_URL = "https://dental-clinic-app.mooo.com/clinic";
+
+//const PROD_BACKEND_API_URL = "https://dental-clinic-app.mooo.com/clinic";
+const PROD_BACKEND_API_URL = "http://ec2-13-48-123-39.eu-north-1.compute.amazonaws.com/clinic"
 const DEV_BACKEND_API_URL = "http://127.0.0.1:8000/clinic";
 
 export const BACKEND_API_URL = 
