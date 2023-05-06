27f02381875234aae3f9a5fa484f7297f345bfe7
927TriponBriana <91745099+917TriponBriana@users.noreply.github.com>
2023-05-06 20:48:07 +0300

    .

--- dental_clinic_beckend/lab5/populate_data.py
+++ dental_clinic_beckend/lab5/populate_data.py
@@ -29,3 +29,26 @@
 #             f.write("'" + fake.name() + "'," + fake.name() + "'," + random.randint(2, 100).__str__() + ",'" + fake.name() + ",'" + fake.name() + '\n')
 #             print(i, "/", n)
 
+from django.core.management.base import BaseCommand
+from faker import Faker
+from django.db import connection
+from app1.Models.PatientModels import Patient
+
+
+class Command(BaseCommand):
+    help = 'Create fake data'
+
+    def handle(self, *args, **options):
+        fake = Faker()
+        cursor = connection.cursor()
+        cursor.execute('''
+            INSERT INTO patients (patient_first_name, patient_last_name, patient_age, patient_country, patient_consultation)
+            SELECT 
+                md5(random()::text),
+                md5(random()::text),
+                md5(random()::text),
+                md5(random()::text),
+                md5(random()::text)
+            FROM generate_series(1, 100)
+        ''')
+        self.stdout.write(self.style.SUCCESS('Successfully created!'))
