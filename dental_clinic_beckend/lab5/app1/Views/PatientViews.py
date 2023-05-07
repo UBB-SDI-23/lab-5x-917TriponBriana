5422190e628af7406e7367d06a5ba28b420091e1
927TriponBriana <91745099+917TriponBriana@users.noreply.github.com>
2023-05-07 21:48:53 +0300

    .

--- dental_clinic_beckend/lab5/app1/Views/PatientViews.py
+++ dental_clinic_beckend/lab5/app1/Views/PatientViews.py
@@ -11,69 +11,18 @@ from ..Serializers.PatientSerializer import PatientSerializer
 from rest_framework import generics
 
 
-class PatientDetail(APIView):
-    def get(self, request):
-        obj = Patient.objects.all()[:100]
-        serializer = PatientSerializer(obj, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def post(self, request):
-        serializer = PatientSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
-
-
-class PatientInfo(APIView):
-    def get(self, request, id):
-        try:
-            obj = Patient.objects.get(id=id)
-        except Patient.DoesNotExist:
-            msg = {"msg" : "Not found"}
-            return Response(msg, status=status.HTTP_404_NOT_FOUND)
-        serializer = PatientSerializer(obj)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def put(self, request, id):
-        try:
-            obj = Patient.objects.get(id=id)
-        except Patient.DoesNotExist:
-            msg = {"msg" : "Not found"}
-            return Response(msg, status=status.HTTP_404_NOT_FOUND)
-        serializer = PatientSerializer(obj, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_205_RESET_CONTENT)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def patch(self, request, id):
-        try:
-            obj = Patient.objects.get(id=id)
-        except Patient.DoesNotExist:
-            msg = {"msg": "Not found"}
-            return Response(msg, status=status.HTTP_404_NOT_FOUND)
-        serializer = PatientSerializer(obj, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_205_RESET_CONTENT)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, id):
-        try:
-            obj = Patient.objects.get(id=id)
-        except Patient.DoesNotExist:
-            msg = {"msg": "Not found"}
-            return Response(msg, status=status.HTTP_404_NOT_FOUND)
-        obj.delete()
-        return Response({"msg" : "Deleted"}, status=status.HTTP_204_NO_CONTENT)
-
-# class PatientView(generics.ListCreateAPIView):
-#     serializer_class = PatientSerializer
+# class PatientDetail(APIView):
+#     def get(self, request):
+#         obj = Patient.objects.all()[:100]
+#         serializer = PatientSerializer(obj, many=True)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
 #
-#     def get_queryset(self):
-#         queryset = Patient.objects.annotate(nb_registers=Count('patient_first_name'))
-#         return queryset
+#     def post(self, request):
+#         serializer = PatientSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
 #
 #
 # class PatientInfo(APIView):
@@ -118,3 +67,54 @@ class PatientInfo(APIView):
 #             return Response(msg, status=status.HTTP_404_NOT_FOUND)
 #         obj.delete()
 #         return Response({"msg" : "Deleted"}, status=status.HTTP_204_NO_CONTENT)
+
+class PatientView(generics.ListCreateAPIView):
+    serializer_class = PatientSerializer
+
+    def get_queryset(self):
+        queryset = Patient.objects.all()
+        return queryset
+
+
+class PatientInfo(APIView):
+    def get(self, request, id):
+        try:
+            obj = Patient.objects.get(id=id)
+        except Patient.DoesNotExist:
+            msg = {"msg" : "Not found"}
+            return Response(msg, status=status.HTTP_404_NOT_FOUND)
+        serializer = PatientSerializer(obj)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, id):
+        try:
+            obj = Patient.objects.get(id=id)
+        except Patient.DoesNotExist:
+            msg = {"msg" : "Not found"}
+            return Response(msg, status=status.HTTP_404_NOT_FOUND)
+        serializer = PatientSerializer(obj, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_205_RESET_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, id):
+        try:
+            obj = Patient.objects.get(id=id)
+        except Patient.DoesNotExist:
+            msg = {"msg": "Not found"}
+            return Response(msg, status=status.HTTP_404_NOT_FOUND)
+        serializer = PatientSerializer(obj, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_205_RESET_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        try:
+            obj = Patient.objects.get(id=id)
+        except Patient.DoesNotExist:
+            msg = {"msg": "Not found"}
+            return Response(msg, status=status.HTTP_404_NOT_FOUND)
+        obj.delete()
+        return Response({"msg" : "Deleted"}, status=status.HTTP_204_NO_CONTENT)
