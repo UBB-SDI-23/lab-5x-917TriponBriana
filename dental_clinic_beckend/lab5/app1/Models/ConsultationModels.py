2232e772111b36a2d73ad8c8d085c208418a32d7
927TriponBriana <91745099+917TriponBriana@users.noreply.github.com>
2023-05-07 15:26:04 +0300

    .

--- dental_clinic_beckend/lab5/app1/Models/ConsultationModels.py
+++ dental_clinic_beckend/lab5/app1/Models/ConsultationModels.py
@@ -2,9 +2,15 @@
 from django.db import models
 
 from ..Models.PatientModels import Patient
+from rest_framework.exceptions import ValidationError
 
 
 class Consultation(models.Model):
+    def clean(self):
+        if self.consultation_price < 0:
+            raise ValidationError(
+                'The consultation price cannot be a negative number!')
+
     consultation_type = models.CharField(max_length=50)
     consultation_price = models.IntegerField()
     consultation_duration = models.IntegerField()
@@ -13,3 +19,7 @@ class Consultation(models.Model):
 
     def __str__(self):
         return f"{self.consultation_type}"
+
+    class Meta:
+        ordering = ['id']
+        indexes = [models.Index(fields=["patient"])]
